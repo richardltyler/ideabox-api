5593d15bdaa3a67933a2e1e4c0a8b9ab05571164
Richard Tyler <richardtyler2011@gmail.com>
2021-06-11 20:04:06 -0600

    Send ideas from seperate file

---
+++ ideas.js
@@ -0,0 +1,15 @@
+module.exports =  [
+  {
+    id: 1, 
+    title: "Sweaters for pugs", 
+    description: "To keep them warm"
+  }, {
+    id: 2,
+    title: "A romcom",
+    description: "But make it ghosts"
+  }, {
+    id: 3, 
+    title: "A game show called Ether/Or", 
+    description: "When you lose you get chloroformed"
+  }
+];
--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 // const express = require('express');
 // const app = express();
+const ideas = require('./ideas.js');
 const express = require('express');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = "Idea Box";
+app.locals.ideas = ideas;
 
 app.get('/', (request, response) => {
   response.send('sah dude')
 });
+
+app.get('/api/v1/ideas', (request, response) => {
+  const ideas = app.locals.ideas;
+
+  response.json({ ideas })
+})
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
