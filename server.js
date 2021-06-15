b2f8c9ee06ff56146ebebfb04502bf1c32d74642
Richard Tyler <richardtyler2011@gmail.com>
2021-06-15 11:53:57 -0600

    Handle errors in delete and post

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-// const express = require('express');
-// const app = express();
 const ideas = require('./data/ideas.js');
 const express = require('express');
 const app = express();
@@ -21,6 +19,9 @@ app.get('/api/v1/ideas', (request, response) => {
 
 app.post('/api/v1/ideas', (request, response) => {
   const newIdea = request.body;
+  const match = app.locals.ideas.find(idea => idea.id === newIdea.id);
+  if (match) return response.status(404).json({ message: 'That idea already exists.'});
+
   app.locals.ideas.push(newIdea);
 
   response.json(newIdea);
@@ -28,12 +29,13 @@ app.post('/api/v1/ideas', (request, response) => {
 
 app.delete('/api/v1/ideas/:id', (request, response) => {
   const { id } = request.params;
-  // const match = app.locals.ideas.find(idea => idea.id === id);
+  const match = app.locals.ideas.find(idea => idea.id == id);
+  if (!match) return response.status(404).json({ message: `No idea found with an id of ${id}`});
 
   const filteredIdeas = app.locals.ideas.filter(idea => idea.id != id);
   app.locals.ideas = filteredIdeas;
 
-  return response.sendStatus(204);
+  return response.sendStatus(204).json({ id });
 })
 
 app.listen(app.get('port'), () => {
